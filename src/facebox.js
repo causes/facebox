16626b4d6d82144d60f0e97476d572edde4a29a5
Greg Hurrell <greg@causes.com>
2011-04-12 09:44:12 -0700

    Sync with changes previously made to vendored copy

--- src/facebox.js
+++ src/facebox.js
@@ -92,7 +92,7 @@
       <div class="popup"> \
         <div class="content"> \
         </div> \
-        <a href="#" class="close"><img src="/facebox/closelabel.png" title="close" class="close_image" /></a> \
+        <a href="#" class="close"><img src="" title="close" class="close_image" /></a> \
       </div> \
     </div>'
     },
